9112f15f147e31fac2f10f2b447fd437dc523157
Barbara Hollinger <barbara.hollinger@web.de>
2016-12-06 15:36:20 +0100

    Add Subjects and terms of subjects.

--- app/src/main/java/hochschule/winki/Subjects.java
+++ app/src/main/java/hochschule/winki/Subjects.java
@@ -5,5 +5,53 @@ package hochschule.winki;
  */
 
 public class Subjects {
-    public static String[] firstSemester = {"Wirtschaftsmathematik 1", "BWL - Grundlagen", "Wirtschaftsinformatik", "Softwareentwicklung 1", "Buchführung"};
+    public static String[] firstSemester = {"Wirtschaftsmathematik 1", "BWL - Grundlagen", "Wirtschaftsinformatik 1", "Softwareentwicklung 1", "Buchführung"};
+    public static final String[] secondSemester = {"Wirtschaftsprivatrecht 1", "Volkswirtschaft", "Statistik und Operations Research", "Wirtschaftsmathematik 2", "Wirtschaftsinformatik 2", "Softwarentwicklung 2";
+    public static final String[] thirdSemester = {"Personal und Organisation", "Datenbanksysteme", "Datenkommunikation", "Softwareengineering 1", "Informationssysteme 1"};
+    public static final String[] fourthSemester = {"Bilanzierung und Steuern", "Informationssysteme 2", "Softwareengineering 2"};
+    //mal schauen ob wir das fünfte und siebte Semester überhaupt brauchen, sollten wir eher rauslassen
+    public static final String[] fifthSemester = {"PBLV Seminar", "PBLV Projekt", "Praxisbericht"};
+    public static final String[] sixthSemester = {"Kosten- Und Leistungsrechnung", "Geschäftsprozesse", "Business Simulation"};
+    public static final String[] seventhSemester = {"Bachelorarbeit"};
+    public static final String[] wpfgWirtschaft = {"Produktionswirtschaft", "Marketing", "Datenschutz"};
+    public static final String[] wpfgIT = {"Algorithmen und Datenstrukturen", "IT-Sicherheit", "Datenmanagement"};
+
+    // 1. Semester
+    public static final String[] wirtschaftsmathematik1 = {"Grenzwerte", "Funktion", "Umkehrfunktion"};
+    public static final String[] bwl = {"Rechnungswesen", "Rechtsform", "Marketing"};
+    public static final String[] wirtschaftsinformatik1 = {"Hardware", "Betriebssystem", "IT-Service-Management"};
+    public static final String[] softwareentwicklung1 = {"Java", "Objektorientierte Programmierung", "Array"};
+    public static final String[] bubi = {"Handelsgesetzbuch", "Umlaufvermögen", "Anlagevermögen"};
+
+    // 2. Semester
+    public static final String[] wpr = {"Schadensersatz", "Kaufvertrag", "Rechtsfähigkeit"};
+    public static final String[] vwl = {"Mikroökonomie", "Makroökonomie", "Bruttoinlandsprodukt"};
+    public static final String[] statistik = {"Histogramm", "Korrelation", "Simplex-Verfahren"};
+    public static final String[] wirtschaftsmathematik2 = {"Vektor", "Matrix", "Diferentialrechnung"};
+    public static final String[] softwareentwicklung2 = {"Exception", "Vererbung", "Test-Driven-Development"};
+    public static final String[] wirtschaftsinformatik2 = {"Cloud Computing", "BigDAta", "NoSQL"};
+
+    //3. Semester
+    public static final String[] personalOrganisation = {"Aufbauorganisation", "Ablauforganisation", "Personalbeschaffung"};
+    public static final String[] datenbanksysteme = {"Datenbankmanagementsystem", "SQL", "Datenunabhängigkeit"};
+    public static final String[] datenkommunikation = {"Routing", "Subnetting", "TCP/IP"};
+    public static final String[] softwareeningineering1 = {"Anforderungen", "Lastenheft", "UML"};
+    public static final String[] informationssysteme1 = {"CRM", "ERP", "Systemarchitektur"};
+
+    //4. Semester
+    public static final String[] bilanzierungSteuern = {"Handelsbilanz", "Steuerbilanz", "Einkommenssteuer"};
+    public static final String[] informationssysteme2 = {"Data-Mining", "SAP", "Clusteranalyse"};
+    public static final String[] softwareengineering2 = {"Versionsverwaltung", "Kanban", "Scrum"};
+
+    //6. Semester
+    public static final String[] klr = {"Gemeinkosten", "Einzelkosten", "Kostenstelle"};
+    public static final String[] businessSimulation = {"Unternehmensbewertung", "Kleine und mittlere Unternehmen", "Startups"};
+
+    //Wahlpflichtfächergruppe Wirtschaft
+    public static final String[] produktionswirtschaft = {"Insourcing", "Outsourcing", "Lean-Management"};
+    public static final String[] datenschutz = {"Datenschutzbeauftragter", "TMG", "TKG"};
+
+    //Wahlpflichtfächergruppe IT
+    public static final String[] itSicherheit = {"Sicherheitsmanagement", "Access Control", "Risikomanagement"};
+    public static final String[] datenmanagement = {"OLAP", "OLTP", "Logdatei"};
 }
