d5f86ed7da1f1413ec6b1d78de85b0671086161f
danielf <daniel.frick@maihiro.com>
2017-01-14 13:11:34 +0100

    JSON request to wikipedia is working now

--- app/src/main/java/hochschule/winki/WikipediaItem.java
+++ app/src/main/java/hochschule/winki/WikipediaItem.java
@@ -1,23 +1,39 @@
 package hochschule.winki;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -26,88 +42,94 @@ import java.util.concurrent.ExecutionException;
 
 public class WikipediaItem  extends AppCompatActivity {
 
+    ProgressDialog pd;
+    TextView txtJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wiki_article);
         TextView title = (TextView) findViewById(R.id.titlearticle);
-        TextView article = (TextView) findViewById(R.id.article);
+        txtJson = (TextView) findViewById(R.id.article);
 
        Bundle b = getIntent().getExtras();
 
         title.setText(b.getString(Main_Activity.ArticleTitleBundleKey));
 
-       /* try {
-            String test = new RetrieveFeedTask().execute("Marketing").get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }*/
+        new JsonTask().execute("https://de.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=Marketing");
     }
 
-    private String getArticle(String articleName) throws IOException, JSONException {
-        return getJSONObjectFromURL("https://de.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + articleName).toString();
-    }
 
-    public static JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException, JSONException {
+    private class JsonTask extends AsyncTask<String, String, String> {
 
-        HttpURLConnection urlConnection = null;
+        protected void onPreExecute() {
+            super.onPreExecute();
 
-        URL url = new URL(urlString);
-
-        urlConnection = (HttpURLConnection) url.openConnection();
-
-        urlConnection.setRequestMethod("GET");
-        urlConnection.setReadTimeout(10000 /* milliseconds */);
-        urlConnection.setConnectTimeout(15000 /* milliseconds */);
-
-        urlConnection.setDoOutput(true);
-
-        urlConnection.connect();
-
-        BufferedReader br=new BufferedReader(new InputStreamReader(url.openStream()));
-
-        char[] buffer = new char[1024];
-
-        String jsonString = new String();
-
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line+"\n");
+            pd = new ProgressDialog(WikipediaItem.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
         }
-        br.close();
 
-        jsonString = sb.toString();
-
-        System.out.println("JSON: " + jsonString);
-
-        return new JSONObject(jsonString);
-    }
-
-    class RetrieveFeedTask extends AsyncTask<String, Void, String> {
-
-        private Exception exception;
-
-
-        @Override
         protected String doInBackground(String... params) {
-            String test = "";
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
             try {
-                getArticle("Marketing");
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+                String id = buffer.toString();
+                id = id.substring(41);
+                JSONObject jsonObj = new JSONObject( buffer.toString());
+                String article = jsonObj.getJSONObject("query").getJSONObject("pages").getJSONObject("").get("extract").toString();
+                return article;
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (JSONException e) {
                 e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            return test;
+            return null;
         }
 
-        protected void onPostExecute(String feed) {
-            // TODO: check this.exception
-            // TODO: do something with the feed
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            txtJson.setText(result);
         }
     }
 }
